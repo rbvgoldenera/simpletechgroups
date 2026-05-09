8422bbda9b26944957849e53eabe032c2cb15c55
Ask I Viki <askiviki.official@gmail.com>
2026-05-09 12:10:28 +0530

    Reorganize Authentication Views Structure
    - Moved login view to views/authorization/login.php
    - Moved register view to views/authorization/register.php
    - Updated Authorization controller view paths
    - Improved authentication module folder organization
    - Cleaned up view structure for better maintainability

--- application/controllers/Authorization.php
+++ application/controllers/Authorization.php
@@ -6,13 +6,13 @@ class Authorization extends CI_Controller {
 	public function register()
 	{
 		$this->load->view('common/header');
-		$this->load->view('register/index');
+		$this->load->view('authorization/register');
 		$this->load->view('common/footer');
 	}
 	public function login()
 	{
 		$this->load->view('common/header');
-		$this->load->view('login/index');
+		$this->load->view('authorization/login');
 		$this->load->view('common/footer');
 	}
 }
